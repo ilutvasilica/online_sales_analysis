c2b0b2c772c7a92f6c9974d75021a96fb6dcd702
Ilut Vasilica <ilutvasilica@yahoo.com>
2025-07-03 13:36:25 +0200

    Added the Product class with display and update methods.

---
+++ product.py
@@ -0,0 +1,17 @@
+
+class Product :
+    def __init__(self,name,price,quantity):
+        self.name = name 
+        self.price = price 
+        self.quantity = quantity
+        
+    def __str__(self):
+        return f"{self.name},{self.price},{self.quantity}"
+    
+    def update_quantity(self,amount):
+        self.quantity +=amount
+    
+
+
+
+        
